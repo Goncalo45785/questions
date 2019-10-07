f72dee00eaf2a90beb367f3320ec14040a6dfd22
Gonçalo Cordeiro <ga.cordeiro93@gmail.com>
2019-10-07 23:27:48 +0100

    Created resources for share functionality

--- app/src/main/java/pt/goncalo/blissquestions/utils/ActivityUtils.java
+++ app/src/main/java/pt/goncalo/blissquestions/utils/ActivityUtils.java
@@ -1,12 +1,28 @@
 package pt.goncalo.blissquestions.utils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
 
 public class ActivityUtils {
     public static final String TAG = Activity.class.getSimpleName();
 
-    public final static boolean isUrlIntent(Intent intent) {
+    public static boolean isUrlIntent(Intent intent) {
         return intent != null && intent.getData() != null;
+    }
+
+    public static void shareUrlByMail(@NonNull Context context,@NonNull String url,@NonNull String title) {
+        if (url.isEmpty()) {
+            return;
+        }
+        Intent sendIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"));
+        sendIntent.putExtra(Intent.EXTRA_TEXT, url);
+        sendIntent.putExtra(Intent.EXTRA_TITLE, title);
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        context.startActivity(shareIntent);
     }
 }
